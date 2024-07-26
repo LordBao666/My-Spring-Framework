1e056658728f96e10662fa502d95dc8c757a9641
LordBao666 <916900021@qq.com>
2024-07-26 14:19:52 +0800

    Finish Spring-Test

--- spring-ioc-java-practice/src/test/java/com/lordbao/SpringTest.java
+++ spring-ioc-java-practice/src/test/java/com/lordbao/SpringTest.java
@@ -3,19 +3,35 @@ package com.lordbao;
 import com.lordbao.config.SpringConfig;
 import com.lordbao.controller.StudentController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * @Author Lord_Bao
  * @Date 2024/7/26 10:44
  * @Version 1.0
  */
+
+@SpringJUnitConfig(value = {SpringConfig.class})
 public class SpringTest {
+    @Autowired
+    private StudentController studentController;
+
     @Test
     public void test(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 //        StudentController studentController = context.getBean("studentController", StudentController.class);
         StudentController studentController = context.getBean(StudentController.class);
+        studentController.findAll();
+    }
+
+    //Import spring-test package first, so you can use  @Annotation SpringJunitConfig.
+    //The  SpringJunitConfig provides a spring environment for you to test, so you can
+    //eliminate redundant code to create the ioc-container. See the difference between
+    //test and test2.
+    @Test
+    public void test2(){
         studentController.findAll();
     }
 }
